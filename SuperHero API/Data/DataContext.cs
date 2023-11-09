e29f9f34a4e57e939975798ae4d4486cc5cc9303
Omkar Karnik <omkarkarnik@Omkars-MBP.lan>
2023-11-08 21:20:17 -0500

    Complete API

---
+++ SuperHero API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SuperHero_API.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+
+		}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=localhost;Database=superherodb;TrustServerCertificate=true;User=sa;Password=Darkemperor@619");
+
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    } 
+
+}
